1fc661a7047a6e8009621baf44ab01546d8b77f2
Aurnab <zubayeraurnab@gmail.com>
2024-01-09 23:45:16 +0600

    Get Single Customer Data By Id / API Created

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // middleware
 app.use(cors());
@@ -34,6 +34,13 @@ async function run() {
     //Get All user data
     app.get("/all-customers", async (req, res) => {
       const result = await CustomerCollection.find().toArray();
+      res.send(result);
+    });
+    // Get Single Customer Data by id
+    app.get("/customer/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await CustomerCollection.find(query).toArray();
       res.send(result);
     });
     // Connect the client to the server	(optional starting in v4.7)
